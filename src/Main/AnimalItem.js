1c00889c820b693719c7cb73322174e895647305
David Francisco <dfrancisco2677@gmail.com>
2022-06-08 20:39:10 -0700

    Added says prop.

--- src/Main/AnimalItem.js
+++ src/Main/AnimalItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function AnimalItem({ name, type }) {
+export default function AnimalItem({ name, type, says }) {
   return (
     <div className="animalDiv">
       <img src={`/animals/${type}.svg`} />
       <p>{name}</p>
+      <p>{says}</p>
     </div>
   );
 }
