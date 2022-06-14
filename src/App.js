1ee5ac1cef814f68aaec9fbbbf5377ad214d3c0f
David Francisco <dfrancisco2677@gmail.com>
2022-06-14 16:03:03 -0700

    moving bg image.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { animals } from './data.js';
 import Main from './Main/main.js';
+import backgroundImage from './background.png';
 
 function App() {
   return (
-    <div>
+    <div className='animalsDiv' style={{ backgroundImage: `url(${backgroundImage})` }}>
       {
         <Main animals={animals}/>
       }
--- src/Main/main.js
+++ src/Main/main.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import AnimalList from './AnimalList.js';
-import backgroundImage from '../background.png';
 
 export default function main({ animals }) {
   return (
-    <div className='Main' style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <div className='Main'>
       {
         <AnimalList animals={ animals } />
       }
